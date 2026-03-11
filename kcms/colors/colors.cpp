5a430ff9e252c468b0880f4c1f7785eaf456cb8a
Enrico Weigelt, metux IT consult <info@metux.net>
2026-03-11 17:18:39 +0100

    kcms: drop non-X11 code paths
    We're always on X11, so no non-X11 code pathes necessary.
    
    Signed-off-by: Enrico Weigelt, metux IT consult <info@metux.net>

--- kcms/colors/colors.cpp
+++ kcms/colors/colors.cpp
@@ -372,9 +372,7 @@ void KCMColors::editScheme(const QString &schemeName, QQuickItem *ctx)
         // However, since we pass the ID to an external process which has no idea of this
         // we need to resolve the actual window we end up showing in.
         if (QWindow *actualWindow = QQuickRenderControl::renderWindowFor(ctx->window())) {
-            if (KWindowSystem::isPlatformX11()) {
                 args << QStringLiteral("--attach") << QString::number(actualWindow->winId());
-            }
         }
     }
 
