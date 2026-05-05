25e45e8f2aa069e75fa74ccfd690df73a7b28fa4
Antti Savolainen <antti.savo@gmail.com>
2026-05-05 22:40:15 +0300

    shellcorona: add TODO to remove binding
    This seem functionally identical to the one in KWin and calls the same Q_SIGNAL "showingDesktopChanged()"

--- shell/shellcorona.cpp
+++ shell/shellcorona.cpp
@@ -172,6 +172,7 @@ void ShellCorona::init()
     dashboardAction->setAutoRepeat(true);
     dashboardAction->setCheckable(true);
     dashboardAction->setIcon(QIcon::fromTheme(QStringLiteral("dashboard-show")));
+    // TODO Plasma 7: Remove Ctrl keybinding in favor Meta+D in KWin
     KGlobalAccel::self()->setGlobalShortcut(dashboardAction, Qt::CTRL | Qt::Key_F12);
 
     checkAddPanelAction();
