KDE/plasma-workspace

shellcorona: add dbus api to grab an image of the desktop
this is a naive implementation that utilizes
qquickitem::grabContainmentImage to get a reasonable image of the
desktop. it is used by xdg-desktop-portal-kde to get a picture and then
heavily blur it to give a visual cue if the user has multiple screens
with different wallpapers.

this image will include applets and the like so it's not exactly just
the wallpaper but rather the desktop containment as a whole. for the
purposes of the xdg-desktop-portal-kde that's largely sufficient.

shell/shellcorona.h
@@ -142,6 +142,22 @@ public:
 
     Q_INVOKABLE bool isScreenUiReady(int screen);
 
+    /*!
+        Must be called from DBus!
+
+        Grabs an image of the desktop containment of the defined screen.
+        \a connector is the name of the screen (as per QScreen::name()).
+        \a width and \a height define the resulting image size.
+        \a targetPath defines where the image should be saved.
+
+        Returns true if the image was saved successfully.
+
+        Note that this function is asynchronous and will send a delayed reply.
+
+        DO NOT WAIT BLOCKINGLY!
+    */
+    [[nodiscard]] bool grabContainmentImage(const QString &connector, int width, int height, const QString &targetPath);
+
 Q_SIGNALS:
     void glInitializationFailed();
     // A preview for this containment has been rendered and saved to disk

Sonic-DE/sonic-workspace

shell/shellcorona.h
@@ -136,6 +136,24 @@ public:
 
     Q_INVOKABLE bool isScreenUiReady(int screen);
 
+    /*!
+        \internal
+
+        Must be called from DBus!
+
+        Grabs an image of the desktop containment of the defined screen.
+        \a name is the name of the screen (as per QScreen::name()).
+        \a width and \a height define the resulting image size.
+        \a targetPath defines where the image should be saved.
+
+        Returns true if the image was saved successfully.
+
+        Note that this function is asynchronous and will send a delayed reply.
+
+        DO NOT WAIT BLOCKINGLY!
+    */
+    [[nodiscard]] bool grabContainmentImage(const QString &name, int width, int height, const QString &targetPath);
+
 Q_SIGNALS:
     void glInitializationFailed();
     // A preview for this containment has been rendered and saved to disk