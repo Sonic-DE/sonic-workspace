KDE/plasma-workspace

Mark all Text/Labels as being PlainText
We have numerous issues with Text processing user input HTML because
of the bad default of textFormat.

This change sets the textFormat on all Text/Labels unless it's otherwise
explicitly set.

applets/digital-clock/configAppearance.qml
@@ -263,6 +263,7 @@ KCMUtils.SimpleKCM {
             }
 
             onLinkActivated: link => Qt.openUrlExternally(link)
+            textFormat: Text.PlainText
         }
 
         Item {

Sonic-DE/sonic-workspace

applets/digital-clock/configAppearance.qml
@@ -263,6 +263,7 @@ KCMUtils.SimpleKCM {
             }
 
             onLinkActivated: link => Qt.openUrlExternally(link)
+            textFormat: Text.StyledText
         }
 
         Item {