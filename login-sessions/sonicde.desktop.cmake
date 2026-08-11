be4a7cc3e5e68b9231bc06b02190c7414e82f93b
Joseph Crowell <joseph.w.crowell@gmail.com>
2026-08-11 13:34:54 +1000

    Change the session entry in Display Managers and Login Managers to SonicDE (Sonic Desktop Environment) and remove defunct Wayland session entries.
    This is the first major step towards us living along side KDE as a separate option.

---
+++ login-sessions/sonicde.desktop.cmake
@@ -0,0 +1,8 @@
+[Desktop Entry]
+Type=XSession
+Exec=${CMAKE_INSTALL_FULL_BINDIR}/startplasma-x11
+TryExec=${CMAKE_INSTALL_FULL_BINDIR}/startplasma-x11
+DesktopNames=KDE
+Name=SonicDE
+Comment=Sonic Desktop Environment
+X-KDE-PluginInfo-Version=${PROJECT_VERSION}
